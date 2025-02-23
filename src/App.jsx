6926873ac9c18b50101db78c588c29adc370b7e8
Eric Williams <beng985@dopeparadigm.com>
2025-02-23 11:14:10 -0600

    Updated App.jsx to test new Commit via xcode

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>  STO Admin Portal</h1>
+      <h1>  STO Admin Portal 2.0</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
